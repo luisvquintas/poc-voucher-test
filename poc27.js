a7cceafd1964e98ce9d48c72844112b5744e1699
Luís Victor Quintas <luisvictorquintas@gmail.com>
2026-01-21 17:22:18 -0300

    Update and rename poc25.js to poc27.js

--- poc27.js
+++ poc27.js
@@ -101,7 +101,7 @@ LoadCheckoutPaymentContext(function (Checkout, PaymentOptions) {
         }
     });
 
-        const PagarmeVoucherPayment = new PaymentOptions.Transparent.DebitPayment({
+        const PagarmeVoucherPayment = new PaymentOptions.Transparent.CardPayment({
         id: "pagarme_payment_voucher",
         name: "Vale alimentação ou refeição",
         scripts: scriptUrl,
